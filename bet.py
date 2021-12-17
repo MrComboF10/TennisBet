038f4702539b9e4cd4b30cf46d956bac7c877a21
Pedro Azevedo <up201806728@fe.up.pt>
2021-12-17 11:19:49 +0000

    Update bet.py

--- bet.py
+++ bet.py
@@ -10,7 +10,41 @@ def calculate_probs(w_elo, l_elo):
     return 1 / (1 + 10**((w_elo - l_elo) / 400))
 
 # def calculate_probs(w_elo, l_elo):
-#     return w_elo / (w_elo + l_elo)
+#     return w_elo / (w_elo + l_elo) bonjour? Essa vida???? Cette vie?!?!?! Ise Buena xd Isto é tenis? Yas, pensavas que era o quê? xd
+# Estou a brincar, eu já mudo para plog xD :(
+# Eu só meti o tenis porque É mais fixe, mas pronto vamos ter de sair da aula xD
+# Até já então xd  <3 SOu capaz de demorar 20 minutos só? tu consegues vir aqui ter sem te perderes!?!?!?!??!?!?!?!?!?!P?!?!?!?!?!?!
+# Isso já não consigo garantir xD
+# O stor deve estar aí na aula a ver teescrever bué rápido e a pensar: Nossa, aquele gajo é que programa, alguém pare o homem que ele está possuido xD
+# Ele foi a uma gaja que tu conheces que obviamente eu já não me lembro do nome dela xd e ela disse:
+# Oh sô pssô eu já fiz o projeto o ano passado, não posso usar o que já fiz? :chorão: Não me acredito xdxdxd
+# Juro que é verdade xD Que gaja estúpida. É uma loira de óculos não? Exatamente xD É a Carolina, é uma brasielira rrrrosqueeee xD
+# Por acaso pareceu me braSileiro <- (ninguém viu)(tou cegoooo) xd o sotaque dela
+# O trabalho de plog hoje está a andar de carago
+# Mesmo nós estamos a progredir de uma maneira que ninguém ia conseguir sequer imaginar xD Estamos a pegar fogo mesmo
+# O meu pc até está a ficar com as teclas encravadas xD As ventoinhas do meu predador até estão ao pics com o ar condicionado xD
+# Agora estás sempre com o ar condicionado ligado? Por acaso nem estou, era só pra piada mesmo xd
+# Pensei que tinhas gostado da ideia e agora já era rotina xD Not, ainda só usei uma vez e foi dessa vez que estavas comigo, nunca mais
+# Ah okz, gostei do facto de eu ter estado presente nesse primeiro acontecimento importante xD <3 <3
+# E não quero tê lo ligado agora para ver como me sinto, se sentir frio é cuvides xd E se sentir calor se calhar é cuvides também
+# Oh diabo, então espero que não tenhas frio nem calor para o resto da tua vida xD
+# coitado do covid, ele também merece viver, não é filho de Deus também?
+# É capaz e se for vamos todos quinar xD A foda é que vou ter de pagar 20 euros por cada noite que ele aqui dormir comigo porque não posso ter convidados xD
+# Que boa xD Eu demorei uma beca a perceber xD Desculpa me, devia ter avisado que esta piada era nivel de scrum master xd
+# O meu nível é só de developer xD
+# Olhei agora para cima, Jesus, quem é que escreveu isto tudo? xd
+# Nós estamos a programar a um ritmo que nem é imaginável xD
+# Houve outra gaja que fez a mesma pergunta ao sô pssô xD Está tudo maluquinho? xd
+# Completamente, aparentemente somo só nós os duros que já estamos habituados ao chumbo xD É um talento desenvolvido ao longo dos anos
+# Exato, a esta altura já devemos estar subdesenvolvidos xD xd Chumbar tornou se rotineiro, é como ir à missa ao domingo xd
+# Que boa xD Espera, lembrei me agora doutra mesmo boa... :olhinhos:
+# :tambores............. xD: era fazer um trabalhinho de plog (tens de ler isso como se fosse eu a dizer daquela maneira especifica xd)
+# Era mesmo isso que fiz e estou a passar uma beca mal xD xd
+# (momento de recuperação) xd
+# Então só vai fazer plog ou tens alguma ideia mais fixe? TENHO, mas se calhar é melhor fazer plog xd
+# Okz, então vou ter de te, infelizmente, expulsar desta sessão momentaneamente para passar à sessão mais rosque >:(
+# Desculpa me, não sou eu que faço as regras o'o (isto era o chorão, mas não correu tão bem como eu estava a pensar XD ADEUS ADEUS) Só vai
+
 
 
 def prob_to_odd(prob):
